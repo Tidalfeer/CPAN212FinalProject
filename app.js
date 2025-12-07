55f6f95665da633e32395470629fd0b036d04e55
Tidalfeer <tylerlee1406@gmail.com>
2025-12-07 18:02:51 -0500

    Update app.js
    added express layout to correct for deployment error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -32,6 +33,9 @@ mongoose.connect(MONGODB_URI, {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
