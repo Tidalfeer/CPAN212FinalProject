33961e35e6d6966d22c966f4b57eed0718cf5907
Tidalfeer <tylerlee1406@gmail.com>
2025-12-07 20:16:58 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -52,10 +52,10 @@ router.post('/logout', (req,res)=>{
 
 router.get('/profile', requireLogin, async (req, res) => {
   try {
-    // Get current user's data
+    
     const user = await User.findById(req.session.user.id);
     
-    // Get movies created by this user
+    
     const Movie = require('../models/Movie');
     const userMovies = await Movie.find({ owner: req.session.user.id });
     
